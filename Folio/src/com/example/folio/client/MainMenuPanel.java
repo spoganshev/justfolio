fd56b779c1febb56544e48b9c0b802ec501210e8
maksi <MI@MI-X220.(none)>
2012-07-17 23:15:57 +0300

    Test commit

--- Folio/src/com/example/folio/client/MainMenuPanel.java
+++ Folio/src/com/example/folio/client/MainMenuPanel.java
@@ -3,12 +3,7 @@ package com.example.folio.client;
 import java.util.logging.Logger;
 
 import com.example.folio.client.component.CustomAnchor;
-import com.example.folio.client.cover.CoverPage;
-import com.example.folio.client.project.ProjectPage;
 import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.LayoutPanel;
 
