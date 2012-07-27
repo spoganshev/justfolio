757d0d7482905b512245a462fda3edb60577f6d2
Sergei Poganshev <spoganshev@gmail.com>
2012-07-27 12:07:50 +0300

    Added getters and setters.

--- Folio/src/com/example/folio/shared/model/ImageItem.java
+++ Folio/src/com/example/folio/shared/model/ImageItem.java
@@ -5,5 +5,21 @@ public class ImageItem extends Item {
 	private Image image;
 	
 	private Boolean stretched;
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public Boolean getStretched() {
+		return stretched;
+	}
+
+	public void setStretched(Boolean stretched) {
+		this.stretched = stretched;
+	}
 	
 }
